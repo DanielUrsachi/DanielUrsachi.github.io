9274d10a7a7fd0fa098bf1d6f80bc68e50529aaf
DanielUrsachi <udanny95@gmail.com>
2016-03-28 01:10:08 +0300

    primul commit la site

---
+++ pp.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+		<title>Your own do-while</title>
+        <link type='text/css' rel='stylesheet' href='style.css'/>
+	</head>
+	<body>
+   <p> <?php
+      class Person {
+          
+      }
+      class Ninja extends Person {
+        // Add your code here...
+        const stealth  = "MAXIMUM";
+      }
+      // ...and here!
+      
+      echo Ninja::stealth;
+     
+      ?></p>
+    </body>
+</html>
